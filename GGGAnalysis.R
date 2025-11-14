239af064d3b6346bd633fbdc3a1cb04e3d1b4745
MichaelBj117 <mikey117@byu.edu>
2025-11-14 11:30:45 -0700

    svm set up

--- GGGAnalysis.R
+++ GGGAnalysis.R
@@ -5,7 +5,7 @@ library(patchwork)
 library(glmnet)
 library(rpart)
 library(embed)
-library(kknn)
+library(kernlab)
 
 
 train_data <- vroom("GitHub/GGG/train.csv")
@@ -15,35 +15,32 @@ ggplot(data = train_data, aes(x = bone_length, y = hair_length)) +
   geom_point()
 
 
-GGG_recipe <- recipe(type ~ . , data = train_data) %>% 
-  step_dummy(color) %>% 
-  step_rm(id) %>% 
-  step_interact(~ has_soul:hair_length +
-                  has_soul:bone_length +
-                  has_soul:rotting_flesh +
-                  hair_length:bone_length +
-                  hair_length:rotting_flesh +
-                  bone_length:rotting_flesh) %>% 
-  step_normalize(all_predictors())
+ggg_recipe <- recipe(type ~ ., data = train_data) |>
+  step_rm(id) |>
+  step_unknown(all_nominal_predictors()) |>
+  step_impute_mode(all_nominal_predictors()) |>
+  step_dummy(all_nominal_predictors()) |>
+  step_zv(all_predictors()) |>
+  step_normalize(all_numeric_predictors())
 
 prepped_recipe <- prep(GGG_recipe)
 baked_data <- bake(prepped_recipe, new_data=train_data)
 ncol(baked_data)
 
-knn_model <- nearest_neighbor(neighbors=tune()) %>% # tune
+svmLinear <- svm_linear(cost=tune()) %>% # set or tune
   set_mode("classification") %>%
-  set_engine("kknn")
+  set_engine("kernlab")
 
-knn_workflow <- workflow() %>% 
+svmL_workflow <- workflow() %>% 
   add_recipe(GGG_recipe) %>%
-  add_model(knn_model) 
+  add_model(svmLinear) 
 
-tuning_grid <- grid_regular(neighbors(),
+tuning_grid <- grid_regular(cost(),
                             levels = 5) ## L^2 total tuning possibilities
 
 folds <- vfold_cv(train_data, v = 5, repeats=1)
 
-CV_results <- knn_workflow %>%
+CV_results <- svmL_workflow %>%
   tune_grid(resamples=folds,
             grid=tuning_grid,
             metrics=metric_set(roc_auc)) #Or leave metrics NULL
@@ -51,7 +48,7 @@ CV_results <- knn_workflow %>%
 bestTune <- CV_results %>%
   select_best(metric = "roc_auc")
 
-final_workflow <- knn_workflow %>%
+final_workflow <- svmL_workflow %>%
   finalize_workflow(bestTune) %>%
   fit(data=train_data)
 
@@ -64,4 +61,4 @@ kaggle_sub <- bind_cols(test_data$id, GGG_predictions[1]) %>%
   rename(id = ...1)
 
 vroom_write(kaggle_sub,
-            file="GitHub/GGG/KNN6.csv", delim=",")
+            file="GitHub/GGG/SVML3.csv", delim=",")
